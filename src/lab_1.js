57e4b12e241cf289871adbc7998bcd95ddaeb045
JHPArkser <96711669+JHPArkser@users.noreply.github.com>
2025-09-22 19:18:13 +0900

    Create lab_1.js

---
+++ src/lab_1.js
@@ -0,0 +1,23 @@
+import createLog from "./log.js";
+
+async function lab_1() {
+  const params = new URLSearchParams(window.location.search);
+  const scheduleMemberProgressNo = params.get("scheduleMemberProgressNo");
+
+  const response = await fetch(
+    "https://safety.konkuk.ac.kr/ushm/edu/contentsViewAviProcessCheckSub",
+    {
+      method: "POST",
+      body: new URLSearchParams({
+        scheduleMemberProgressNo,
+        currentTime: "23",
+        isEnd: "true",
+        isMobile: "false",
+      }),
+    },
+  );
+
+  createLog(response.ok);
+}
+
+export default lab_1;
